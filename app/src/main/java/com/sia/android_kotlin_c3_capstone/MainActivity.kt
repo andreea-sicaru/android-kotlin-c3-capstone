63c8105e04a93e8006467d4827a7ba0a948ef344
Andreea Sicaru <andreea.sicaru@uber.com>
2024-08-08 12:43:39 +0300

    Initial project setup

---
+++ app/src/main/java/com/sia/android_kotlin_c3_capstone/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.sia.android_kotlin_c3_capstone
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+}
