da4201b143fce3d6c3112e69af1a7da389e84486
Andreea Sicaru <andreea.sicaru@uber.com>
2024-08-09 20:29:15 +0300

    Delete unused code

--- app/src/main/java/com/sia/android_kotlin_c3_capstone/DetailActivity.kt
+++ app/src/main/java/com/sia/android_kotlin_c3_capstone/DetailActivity.kt
@@ -1,5 +1,6 @@
 package com.sia.android_kotlin_c3_capstone
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.sia.android_kotlin_c3_capstone.databinding.ActivityDetailBinding
@@ -12,13 +13,10 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
-//        setSupportActionBar(binding.toolbar)
+
+        binding.fileName.text = intent.getStringExtra("title")
+        binding.status.text = intent.getIntExtra("status", 0).toString()
 
 
-
-//        savedInstanceState?.let {
-            binding.fileName.text = intent.getStringExtra("title")
-            binding.status.text = intent.getIntExtra("status", 0).toString()
-//        }
     }
 }
